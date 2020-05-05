edab403cb8b3b4389b570ef53e1609a1ab4bd60a
GuillaumeDIONIS <21909286@etu.unicaen.fr>
2020-05-05 17:39:24 +0200

    exercice_2 : ex2_début

---
+++ td1/ex2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+<title>Exercice 2</title>
+<meta charset="utf-8">
+</head>
+<body>
+<?php
+                
+         $size = $_GET["size"];
+       
+         $color = $_GET["color"];
+       
+         $message = $_GET["message"];
+       
+      
+       echo 'Le message souhaité est le suivant  : <div style="font-size:'.$size.'px; color:'.$color.'"> '.$message.'</div>';
+       
+           
+
+?>
+       
+       <p><a href = "ex2.php?message=Message_1&size=15&color=%23ff0000">"Message_1" en rouge + taille 15px</a></p>
+
+       <p><a href = "ex2.php?message=Message_2&size=30&color=%2327d909">"Message_2" en vert + taille 30px</a></p>
+
+       
+       <p><a href = "ex2.php?message=Message_3&size=50&color=%230000ff">"Message_3" en bleu + taille 50px</a></p>
+       
+       <form method="GET">
+              <label for="size">Size : </label>
+              <input type="number" value="10" name="size" id="size">
+              
+              
+              <label for="color">Color : </label>
+              <input type="color" value="" name="color" id="color">
+              
+              
+              <label for="message">Message : </label>
+              <input type="text" value="" name="message" id="message">
+              <input type="submit" value="Valider">
+              
+       </form>
+       
+       </body>
+</html>
